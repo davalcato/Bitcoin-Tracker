ae5c1eacf086e166ef532b8bff8ef1efb357071c
davalcato <davalcato@yahoo.com>
2021-06-30 19:15:37 -0700

    Added the UITableViewDelegate, UITableViewDataSource to create the tableview on the viewController. Along with the title = "DogeCoin"

--- Bitcoin/ViewController.swift
+++ Bitcoin/ViewController.swift
@@ -7,11 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// UI
+// MVVM
+// API Call
+// Pull to Refresh
+
+
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // Create tableview fetch data
+    private let tableView: UITableView = {
+        let table = UITableView()
+        // Register
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "DogeCoin"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    // Override viewdidlayOut
+    override func viewDidLayoutSubviews() {
+        // tableView frame
+        tableView.frame = view.bounds
+    }
+    
+    // Required methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Dogecoin"
+        
+        return cell
     }
 
 
